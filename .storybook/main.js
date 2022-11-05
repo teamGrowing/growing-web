97f73187902b8886d669af61bf31d8de6efa12e7
Yeonju Jo <yjj0287@gmail.com>
2022-11-05 19:37:11 +0900

    Initial setting

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  stories: [
+    "../src/components/common/**/*.stories.mdx",
+    "../src/components/common/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, ".."),
+      "node_modules",
+      "styles",
+    ];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      components: path.resolve(__dirname, "../src/components"),
+      assets: path.resolve(__dirname, "../src/assets"),
+    };
+    return config;
+  },
+};
