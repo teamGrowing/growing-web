64911de341085c360b617b83b844acc44c14aefd
mjmj0217 <mjmj0217@naver.com>
2022-11-16 00:11:58 +0900

    desgin(BottomNavigation): fix bottom navigation

--- src/components/common/BottomNavigation.tsx
+++ src/components/common/BottomNavigation.tsx
@@ -1,11 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import NavItem from "./NavItem";
-import { ReactComponent as AlbumSvg } from "../../assets/icons/navbar/navbarAlbum.svg";
-import { ReactComponent as ChattingSvg } from "../../assets/icons/navbar/navbarChatting.svg";
-import { ReactComponent as HomeSvg } from "../../assets/icons/navbar/navbarHome.svg";
-import { ReactComponent as CalendarSvg } from "../../assets/icons/navbar/navbarCalendar.svg";
-import { ReactComponent as MoreSvg } from "../../assets/icons/navbar/navbarMore.svg";
+import NavItem from './NavItem';
+import { ReactComponent as AlbumSvg } from '../../assets/icons/navbar/navbarAlbum.svg';
+import { ReactComponent as ChattingSvg } from '../../assets/icons/navbar/navbarChatting.svg';
+import { ReactComponent as HomeSvg } from '../../assets/icons/navbar/navbarHome.svg';
+import { ReactComponent as CalendarSvg } from '../../assets/icons/navbar/navbarCalendar.svg';
+import { ReactComponent as MoreSvg } from '../../assets/icons/navbar/navbarMore.svg';
 
 function BottomNavigation() {
   return (
@@ -17,7 +17,7 @@ function BottomNavigation() {
         <NavItem linkTo="/chatting">
           <ChattingSvg width="46" height="45" className="svg" />
         </NavItem>
-        <NavItem linkTo="/home">
+        <NavItem linkTo="/">
           <HomeSvg width="46" height="45" className="svg" />
         </NavItem>
         <NavItem linkTo="/calendar">
@@ -30,16 +30,16 @@ function BottomNavigation() {
     </Nav>
   );
 }
-
 const Nav = styled.nav`
   width: 100%;
   text-align: center;
+  position: fixed;
+  bottom: 0;
 `;
 const NavItems = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 320px;
-  margin: 0 auto;
+  margin: 0 10px;
 `;
 
 export default BottomNavigation;
