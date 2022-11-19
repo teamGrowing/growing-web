9ad693bf501836ec0707060e363d1c49f8055537
mjmj0217 <mjmj0217@naver.com>
2022-11-19 11:29:34 +0900

    feat: add default theme to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,17 @@
+import { addDecorator } from '@storybook/react';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+import { ThemeProvider } from 'styled-components';
+import { myTheme } from '../src/styles/theme/DefaultTheme';
+
+const themes = [myTheme];
+addDecorator(withThemesProvider(themes), ThemeProvider);
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
