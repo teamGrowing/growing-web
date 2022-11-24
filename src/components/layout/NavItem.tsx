5e444e3c376841244dd0e7533bb6e5fa4357d814
mjmj0217 <mjmj0217@naver.com>
2022-11-24 23:31:42 +0900

    fix: remove duplicate declaration

--- src/components/layout/NavItem.tsx
+++ src/components/layout/NavItem.tsx
@@ -8,16 +8,6 @@ type NavItemProp = {
   linkTo: string;
 };
 
-const SVGStyle = styled.div`
-  .${ACTIVE_CLASSNAME} > svg path {
-    stroke: ${({ theme }) => theme.color.pink600};
-  }
-  .${ACTIVE_CLASSNAME} > svg > path:last-of-type {
-    fill: ${({ theme }) => theme.color.pink600};
-    stroke: none;
-  }
-`;
-
 function NavItem({ children, linkTo }: PropsWithChildren<NavItemProp>) {
   return (
     <SVGStyle>
