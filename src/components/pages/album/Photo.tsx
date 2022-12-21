f8b68b253212549b2a535380e10fdf8ac1bbba90
mjmj0217 <mjmj0217@naver.com>
2022-12-21 21:02:41 +0900

    feat: add photo component

---
+++ src/components/pages/album/Photo.tsx
@@ -0,0 +1,54 @@
+import styled, { css } from 'styled-components';
+import { useState } from 'react';
+import checkIcon from '../../../assets/icons/albumPage/Check.png';
+
+const PhotoBox = styled.div<{ isSelected: boolean; imgUrl: string }>`
+  position: relative;
+  margin: 3px;
+
+  background: ${({ isSelected, imgUrl }) =>
+    isSelected
+      ? css`linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${imgUrl})`
+      : css`url(${imgUrl})`};
+  background-size: cover;
+
+  filter: drop-shadow(0px 0px 4px rgba(151, 71, 255, 0.2));
+  border-radius: 6px;
+
+  aspect-ratio: 1/1;
+  flex: 0 0 calc((100% / 3) - 6px);
+`;
+
+const CheckIcon = styled.img`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+
+  padding: 7px 6px;
+
+  width: 34px;
+  height: 34px;
+
+  flex-grow: 0;
+  z-index: 4;
+`;
+
+type PhotoProps = {
+  src: string;
+};
+
+function Photo({ src }: PhotoProps) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const clickHandler = () => {
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <PhotoBox onClick={clickHandler} isSelected={isSelected} imgUrl={src}>
+      {isSelected && <CheckIcon src={checkIcon} alt="선택됨" />}
+    </PhotoBox>
+  );
+}
+
+export default Photo;
