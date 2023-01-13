51b43a6a7897daba7ef8f67213fb728c029bf161
mjmj0217 <mjmj0217@naver.com>
2023-01-13 17:43:37 +0900

    feat: add gallery-new album page

---
+++ src/pages/gallery/NewAlbumPage.tsx
@@ -0,0 +1,70 @@
+import PhotoScroll from '../../components/pages/gallery/PhotoScroll';
+import PhotoDto from '../../types/gallery/Photo.dto';
+
+function NewAlbumPage() {
+  const dummyPhotos: PhotoDto[] = [
+    {
+      id: '1',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '2',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '3',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '4',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '5',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '6',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '7',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '8',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '9',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+    {
+      id: '10',
+      url: 'https://picsum.photos/id/237/200/300',
+      createdAt: '2000',
+      name: 'string',
+    },
+  ];
+  return <PhotoScroll photos={dummyPhotos} />;
+}
+
+export default NewAlbumPage;
