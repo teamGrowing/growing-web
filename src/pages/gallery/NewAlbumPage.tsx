4715f0de2c0fa050183fc5f73fc934403b64189b
mjmj0217 <mjmj0217@naver.com>
2023-01-13 23:07:25 +0900

    feat: add new album page context

--- src/pages/gallery/NewAlbumPage.tsx
+++ src/pages/gallery/NewAlbumPage.tsx
@@ -1,7 +1,12 @@
+import { useRef, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PhotoScroll from '../../components/pages/gallery/PhotoScroll';
 import PhotoDto from '../../types/gallery/Photo.dto';
+import DataContext from './context';
 
 function NewAlbumPage() {
+  const navigate = useNavigate();
+  const selectedPhotos = useRef<string[]>([]);
   const dummyPhotos: PhotoDto[] = [
     {
       id: '1',
@@ -64,7 +69,32 @@ function NewAlbumPage() {
       name: 'string',
     },
   ];
-  return <PhotoScroll photos={dummyPhotos} />;
+  const addToList = (photoId: string) => {
+    selectedPhotos.current.push(photoId);
+  };
+  const removeFromList = (photoId: string) => {
+    const idx = selectedPhotos.current.findIndex((id) => id === photoId);
+    selectedPhotos.current.splice(idx, 1);
+  };
+  const ctxValue = useMemo(() => {
+    return {
+      selectingAvailable: true,
+      addToList,
+      removeFromList,
+    };
+  }, []);
+
+  return (
+    <DataContext.Provider value={ctxValue}>
+      <PhotoScroll
+        photos={dummyPhotos}
+        onAdd={() => {}}
+        onCancel={() => {
+          navigate(-1);
+        }}
+      />
+    </DataContext.Provider>
+  );
 }
 
 export default NewAlbumPage;
