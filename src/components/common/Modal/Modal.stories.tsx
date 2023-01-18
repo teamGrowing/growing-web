655d43be749c0ff7f65ac5a8539fc2e0e61257f3
Yeonju Jo <yjj0287@gmail.com>
2023-01-19 06:29:36 +0900

    feat: add modal component stories

---
+++ src/components/common/Modal/Modal.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Modal from './Modal';
+
+export default {
+  title: 'growing design system/Modal',
+  component: Modal,
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'title',
+  description: 'description',
+};
+
+export const MainSubAction = Template.bind({});
+MainSubAction.args = {
+  title: 'title',
+  description: 'description',
+  mainAction: '확인',
+  subAction: '취소',
+  onMainAction: () => window.alert('확인되었습니다!'),
+  onSubAction: () => window.alert('취소하였습니다!'),
+};
+
+export const Overflow = Template.bind({});
+Overflow.args = {
+  title:
+    'titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle',
+  description: `description description description description\ndescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription`,
+};
+
+export const Album = Template.bind({});
+Album.args = {
+  description: `해당 파일을 앨범에서\n제거하시겠습니까,\n영구 삭제하시겠습니까?`,
+  mainAction: '앨범에서 제거',
+  subAction: '영구 삭제',
+};
