08778e43b0519e2b4514e5909bb9f1d809859f0e
Yeonju Jo <yjj0287@gmail.com>
2023-01-26 21:07:37 +0900

    feat: add bottom sheet component

---
+++ src/components/common/Modal/ModalBottomSheet/BottomSheetMenu.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export interface BottomSheetMenuProps {
+  children: React.ReactNode;
+}
+
+const Menu = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 8px;
+  padding: 12px 0;
+  font-family: 'PretendardRegular';
+  font-size: 16px;
+`;
+
+export default function BottomSheetMenu({ children }: BottomSheetMenuProps) {
+  return <Menu>{children}</Menu>;
+}
