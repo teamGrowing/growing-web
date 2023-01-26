d2ea03730358e0c34d1979715327e5369288043d
Yeonju Jo <yjj0287@gmail.com>
2023-01-26 19:14:45 +0900

    design: change bottom navigation bar css using custom icon component

--- src/components/layout/BottomNavigation.tsx
+++ src/components/layout/BottomNavigation.tsx
@@ -1,11 +1,7 @@
 import styled from 'styled-components';
 
 import NavItem from './NavItem';
-import { ReactComponent as AlbumSvg } from '../../assets/icons/navbar/navbarAlbum.svg';
-import { ReactComponent as ChattingSvg } from '../../assets/icons/navbar/navbarChatting.svg';
-import { ReactComponent as HomeSvg } from '../../assets/icons/navbar/navbarHome.svg';
-import { ReactComponent as CalendarSvg } from '../../assets/icons/navbar/navbarCalendar.svg';
-import { ReactComponent as MoreSvg } from '../../assets/icons/navbar/navbarMore.svg';
+import Icon from '../common/Icon/Icon';
 
 const Nav = styled.nav`
   width: 100%;
@@ -21,7 +17,6 @@ const NavItems = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 16px 16px 20px;
-  gap: 22px;
   isolation: isolate;
 
   position: absolute;
@@ -33,25 +28,36 @@ const NavItems = styled.div`
   background: ${(props) => props.theme.color.white};
   border-top: 1px solid ${(props) => props.theme.color.gray100};
 `;
+const Label = styled.p`
+  text-align: center;
+  font-family: 'PretendardRegular';
+  font-size: 14px;
+  line-height: 17px;
+`;
 
 function BottomNavigation() {
   return (
     <Nav>
       <NavItems>
         <NavItem linkTo="/gallery">
-          <AlbumSvg width="46" height="45" className="svg" />
+          <Icon icon="IconPhotoHeart" />
+          <Label className="text-gradient400">앨범</Label>
         </NavItem>
         <NavItem linkTo="/chatting">
-          <ChattingSvg width="46" height="45" className="svg" />
+          <Icon icon="IconChat" />
+          <Label className="text-gradient400">채팅</Label>
         </NavItem>
         <NavItem linkTo="/">
-          <HomeSvg width="46" height="45" className="svg" />
+          <Icon icon="IconHome" />
+          <Label className="text-gradient400">홈</Label>
         </NavItem>
         <NavItem linkTo="/calendar">
-          <CalendarSvg width="46" height="45" className="svg" />
+          <Icon icon="IconCalendar" />
+          <Label className="text-gradient400">일정</Label>
         </NavItem>
         <NavItem linkTo="/more">
-          <MoreSvg width="46" height="45" className="svg" />
+          <Icon icon="IconBar" />
+          <Label className="text-gradient400">더보기</Label>
         </NavItem>
       </NavItems>
     </Nav>
