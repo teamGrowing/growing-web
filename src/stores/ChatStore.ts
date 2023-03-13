a273ffffd3cbcdfdd57da5a739394e7ee1a57771
Yeonju Jo <yjj0287@gmail.com>
2023-03-13 22:24:55 +0900

    feat: add chatting ui store

---
+++ src/stores/ChatStore.ts
@@ -0,0 +1,45 @@
+import { makeAutoObservable } from 'mobx';
+import { ParentChildChattingDto } from '../types/chat/Chatting.dto';
+
+const CHAT_MODE = {
+  Default: 'Default',
+  Reply: 'Reply',
+  Context: 'Context',
+  longChat: 'LongChat',
+  Menu: 'Menu',
+  Gallery: 'Gallery',
+  Camera: 'Camera',
+  Voice: 'Voice',
+  Map: 'Map',
+} as const;
+export type ChatType = typeof CHAT_MODE[keyof typeof CHAT_MODE];
+
+type Mode = {
+  mode: ChatType;
+  chat?: ParentChildChattingDto;
+};
+
+class ChatStore {
+  chatMode: Mode = {
+    mode: 'Default',
+  };
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setChatMode(data: Mode) {
+    this.chatMode = data;
+  }
+
+  clear() {
+    this.chatMode = {
+      mode: 'Default',
+      chat: undefined,
+    };
+  }
+}
+
+const chatStore = new ChatStore();
+
+export default chatStore;
