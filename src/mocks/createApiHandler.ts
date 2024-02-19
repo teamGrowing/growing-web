db325adf47c0c68adb0a83405c5989a4a122d1ba
Yeonju Jo <yjj0287@gmail.com>
2024-02-19 19:02:20 +0900

    ✨ feat: createApiHandler에 parameter 추가
    - query parameter를 얻어올 수 있도록 수정
     - api 요청 성공 후 수행할 로직들을 정의해두기 위한 함수 선언

--- src/mocks/createApiHandler.ts
+++ src/mocks/createApiHandler.ts
@@ -7,9 +7,15 @@ type ResponseData<TResponse> = {
   [status: number]: NullableResponse<TResponse>;
 };
 
-type RequestHandler<TParams, TResponse> = (
-  params: TParams
+type RequestHandler<TParams, TRequest, TResponse> = (
+  params: TParams,
+  searchParams: TRequest
 ) => ResponseData<TResponse>;
+
+type AfterRequest<TParams, TRequest> = (
+  params: TParams,
+  searchParams: TRequest
+) => void;
 
 export const createApiHandler = <
   TParams extends PathParams,
@@ -18,17 +24,24 @@ export const createApiHandler = <
 >(
   path: string,
   method: keyof typeof http,
-  handleRequest: RequestHandler<TParams, TResponse>
+  handleRequest: RequestHandler<TParams, TRequest, TResponse>,
+  postProcess?: AfterRequest<TParams, TRequest>
 ) => {
   return http[method]<TParams, TRequest, TResponse>(path, async (req) => {
     const params = req.params as TParams;
-    const responseData = handleRequest(params);
+    const url = new URL(req.request.url);
+    const searchParams = url.searchParams as unknown as TRequest;
+    const responseData = handleRequest(params, searchParams);
 
     const handler = handlerInfoManager.getHandlerInfo(path, method);
     const delayTime = handler?.delayTime || 0;
     const responseStatus = handler?.status || 200;
 
     await delay(delayTime);
+
+    if (responseStatus !== 400) {
+      postProcess?.(params, searchParams);
+    }
     return HttpResponse.json(responseData[responseStatus], {
       status: responseStatus,
     });
