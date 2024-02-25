b5795732fa50a89ae2d28551601d4901188c118f
Yeonju Jo <yjj0287@gmail.com>
2024-02-26 00:42:14 +0900

    ♻️ refactor: 채팅 메시지 업데이트 로직 리팩토링
    - 새 채팅 메시지에 대한 쿼리 데이터 무효화 대신 직접 업데이트로 변경
    - 무한 쿼리 데이터 구조의 첫 페이지에 새 채팅 메시지 데이터 추가 구현
    - 기존 데이터가 없는 경우를
    올바르게 처리

--- src/pages/chat/hooks/useReactQuerySubscription.ts
+++ src/pages/chat/hooks/useReactQuerySubscription.ts
@@ -1,10 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { useEffect } from 'react';
-import { useQueryClient } from '@tanstack/react-query';
+import { InfiniteData, useQueryClient } from '@tanstack/react-query';
 import queryKeys from 'libs/react-query/queryKeys';
 import SOCKET_KEY from 'constants/socketKeys';
-import { RoomInfo, ChattingDto, CreateChattingDto } from 'models/chat';
+import {
+  RoomInfo,
+  ChattingDto,
+  CreateChattingDto,
+  ParentChildChattingDto,
+} from 'models/chat';
 import { socket } from 'mocks/socket';
 
 export default function useReactQuerySubscription({
@@ -33,7 +36,33 @@ export default function useReactQuerySubscription({
     });
 
     socket.on(SOCKET_KEY.GET_CHAT, (res: ChattingDto) => {
-      queryClient.invalidateQueries(queryKeys.chatKeys.all);
+      const receivedData: ParentChildChattingDto = {
+        parentChatting: res,
+        childChatting: null,
+      };
+
+      queryClient.setQueryData<
+        InfiniteData<InfiniteData<ParentChildChattingDto>>
+      >(queryKeys.chatKeys.all, (oldData: any) => {
+        if (!oldData) {
+          return {
+            pages: [{ data: [receivedData] }],
+            pageParams: [null],
+          };
+        }
+
+        const updatedFirstPageData = {
+          ...oldData.pages[0],
+          data: [...oldData.pages[0].data, receivedData],
+        };
+
+        const updatedPages = [updatedFirstPageData, ...oldData.pages.slice(1)];
+
+        return {
+          ...oldData,
+          pages: updatedPages,
+        };
+      });
 
       if (res.Writer.id === userId) {
         let t: number = 100;
@@ -51,7 +80,8 @@ export default function useReactQuerySubscription({
     });
 
     return () => {
-      socket.disconnect();
+      socket.off(SOCKET_KEY.GET_INFO);
+      socket.off(SOCKET_KEY.GET_CHAT);
     };
   }, [coupleId, userId]);
 
