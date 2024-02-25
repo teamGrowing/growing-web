db515194f96d9a769f0c4a639fcd9446aef1b057
Yeonju Jo <yjj0287@gmail.com>
2024-02-26 02:38:52 +0900

    ♻️ refactor: mockedSocket 클래스 리팩토링
    - 이벤트 리스너 초기화 로직을 별도의 메서드로 분리하여 구조화
    - 채팅 데이터 생성 로직을 createChatData 메서드로 분리
    - emojiId가 없는 경우 처리 로직 개선

--- src/mocks/chat/chatHandler.ts
+++ src/mocks/chat/chatHandler.ts
@@ -29,64 +29,77 @@ let chatData: ParentChildChattingDto[] = originData;
 const findChatData = (id: string): ChattingDto =>
   chatData.filter((chat) => chat.parentChatting.id === id)[0].parentChatting;
 
-// socket 관련
+/**
+ * socket mocking 클래스
+ */
 class MockedSocket {
   private server;
 
   constructor() {
     this.server = new Server();
-
-    this.server.on('connection', () => {
-      console.log('User connected');
-
-      this.server.on('disconnection', () => {
-        console.log('User disconnected');
-      });
-
-      this.server.on(SOCKET_KEY.CREATE_CHAT, (dto: CreateChattingDto) => {
-        console.log('get chat');
-
-        const data: ChattingDto = {
-          id: uuidv4(),
-          content: dto.content,
-          emojiUrl: dto.emojiId && dto.emojiId,
-          imageUrls: new Array(dto.imageIds.length).fill(ImgDefaultProfile),
-          videoUrls: [],
-          voiceMsgUrls: [],
-          createdAt: new Date(),
-          isMine: true,
-          Writer: {
-            id: '1',
-            name: '연주',
-            imageUrl: ImgDefaultProfile,
-          },
-        };
-        chatData = [...chatData, { parentChatting: data, childChatting: null }];
-
-        this.server.emit(SOCKET_KEY.GET_CHAT, data);
-      });
-    });
-
+    this.initializeListeners();
     this.connect();
   }
 
-  private connect() {
+  private initializeListeners() {
+    this.server.on('connection', () => {
+      console.log('User connected');
+      this.initializeDisconnection();
+      this.initializeCreateChat();
+    });
+  }
+
+  private initializeDisconnection() {
+    this.server.on('disconnection', () => {
+      console.log('User disconnected');
+    });
+  }
+
+  private initializeCreateChat() {
+    this.server.on(SOCKET_KEY.CREATE_CHAT, (dto: CreateChattingDto) => {
+      console.log('get chat');
+      const data = this.createChatData(dto);
+      chatData.push({ parentChatting: data, childChatting: null });
+      this.server.emit(SOCKET_KEY.GET_CHAT, data);
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private createChatData(dto: CreateChattingDto): ChattingDto {
+    return {
+      id: uuidv4(),
+      content: dto.content,
+      emojiUrl: dto.emojiId || null,
+      imageUrls: Array(dto.imageIds.length).fill(ImgDefaultProfile),
+      videoUrls: [],
+      voiceMsgUrls: [],
+      createdAt: new Date(),
+      isMine: true,
+      Writer: {
+        id: '1',
+        name: '연주',
+        imageUrl: ImgDefaultProfile,
+      },
+    };
+  }
+
+  connect() {
     this.server.socketClient.emit('connection');
   }
 
-  public disconnect() {
+  disconnect() {
     this.server.socketClient.emit('disconnection');
   }
 
-  public on(event: string, callback: (res: any) => void) {
+  on(event: string, callback: (res: any) => void) {
     this.server.socketClient.on(event, callback);
   }
 
-  public off(event: string) {
+  off(event: string) {
     this.server.socketClient.off(event);
   }
 
-  public emit(event: string, dto: any) {
+  emit(event: string, dto: any) {
     this.server.socketClient.emit(event, dto);
   }
 }
@@ -386,10 +399,8 @@ export const getPhotoDetailHandler = createApiHandler<
   path: '/couples/:coupleId/chattings/:chattingId/photos',
   method: 'get',
   requestHandler: ({ chattingId }) => {
-    console.log('111');
     const { id, createdAt, Writer, imageUrls, videoUrls } =
       findChatData(chattingId);
-    console.log('222');
 
     const data: ChatPhotoDto = {
       id,
